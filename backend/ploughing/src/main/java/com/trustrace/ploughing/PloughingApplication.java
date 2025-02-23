93850e1562b23f99c65c1815dd4dd0a633e742c8
HARISH S <hareeshseen@gmail.com>
2025-02-23 11:00:40 +0530

    first commit

---
+++ backend/ploughing/src/main/java/com/trustrace/ploughing/PloughingApplication.java
@@ -0,0 +1,13 @@
+package com.trustrace.ploughing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PloughingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PloughingApplication.class, args);
+	}
+
+}
---
+++ backend/ploughing/src/test/java/com/trustrace/ploughing/PloughingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trustrace.ploughing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PloughingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
